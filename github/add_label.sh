1c3499510ecec4cd841445a10dc9f8c5c7357e7c
George Oastler <goastler4@gmail.com>
2023-03-30 10:51:37 +0100

    add label script for github

---
+++ github/add_label.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -u
+
+label="$1"
+desc="$2"
+colour="$3" # without the leading "#"
+
+for repo in common ops protocol provider scripts contract procaptcha procaptcha-react workspaces datasets api demo-nft-marketplace client-example-server client-example dapp-example integration ; do
+	echo "$repo"
+	gh api \
+	  --method POST \
+	  -H "Accept: application/vnd.github+json" \
+	  -H "X-GitHub-Api-Version: 2022-11-28" \
+	  "/repos/prosopo/${repo}/labels" \
+	  -f name="$label" \
+	 -f description="$desc" \
+	 -f color="$colour" \
+	 | echo
+done
+
